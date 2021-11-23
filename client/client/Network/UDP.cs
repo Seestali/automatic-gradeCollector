9573c3bf68e843b6115c21cd72ee428b838ad1f6
Henrik Kaltenbach <henrik.kaltenbach49@gmail.com>
2021-11-23 19:04:03 +0100

    Add UDP send and receive functionality. Not fully done yet

--- client/client/Network/UDP.cs
+++ client/client/Network/UDP.cs
@@ -1,13 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace client.Network
 {
-    class UDP
+    public class UDP
     {
+        private static int port = 42069;
+        private static string serverAddress = "";
+        private static UdpClient udpReceiver = new UdpClient(port);
+        private static IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(serverAddress), port);
 
+        private UDP() { }
+
+        public void Send(byte[] datagram)
+        {
+            using (UdpClient c = new UdpClient(port))
+                c.SendAsync(datagram, datagram.Length, serverEP);
+        }
+
+        public void Receive()
+        {
+            udpReceiver.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+        }
+
+        private void ReceiveCallback(IAsyncResult result)
+        {
+            byte[] received = udpReceiver.EndReceive(result, ref serverEP);
+            udpReceiver.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+            // TODO: Received data must be forwarded
+        }
     }
 }
