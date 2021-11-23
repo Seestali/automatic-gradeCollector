b3edea28bd467b872576de9267ed8c499cc62112
Seestali <seebasti@gmx.de>
2021-11-23 18:54:26 +0100

    Changed a few GUI settings

--- client/client/Forms/Login.Designer.cs
+++ client/client/Forms/Login.Designer.cs
@@ -29,72 +29,88 @@ namespace client
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbPassword = new System.Windows.Forms.Label();
-            this.lbUser = new System.Windows.Forms.Label();
-            this.tbUser = new System.Windows.Forms.TextBox();
+            this.credentialsBox = new System.Windows.Forms.GroupBox();
             this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
+            this.lbUser = new System.Windows.Forms.Label();
+            this.credentialsBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lbPassword
+            // credentialsBox
             // 
-            this.lbPassword.AutoSize = true;
-            this.lbPassword.Location = new System.Drawing.Point(141, 214);
-            this.lbPassword.Name = "lbPassword";
-            this.lbPassword.Size = new System.Drawing.Size(50, 13);
-            this.lbPassword.TabIndex = 3;
-            this.lbPassword.Text = "Passwort";
-            // 
-            // lbUser
-            // 
-            this.lbUser.AutoSize = true;
-            this.lbUser.Location = new System.Drawing.Point(141, 188);
-            this.lbUser.Name = "lbUser";
-            this.lbUser.Size = new System.Drawing.Size(38, 13);
-            this.lbUser.TabIndex = 4;
-            this.lbUser.Text = "Nutzer";
-            // 
-            // tbUser
-            // 
-            this.tbUser.Location = new System.Drawing.Point(197, 185);
-            this.tbUser.Name = "tbUser";
-            this.tbUser.Size = new System.Drawing.Size(100, 20);
-            this.tbUser.TabIndex = 0;
+            this.credentialsBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.credentialsBox.Controls.Add(this.lbUser);
+            this.credentialsBox.Controls.Add(this.btnLogin);
+            this.credentialsBox.Controls.Add(this.tbUser);
+            this.credentialsBox.Controls.Add(this.lbPassword);
+            this.credentialsBox.Controls.Add(this.tbPassword);
+            this.credentialsBox.Location = new System.Drawing.Point(130, 160);
+            this.credentialsBox.Name = "credentialsBox";
+            this.credentialsBox.Size = new System.Drawing.Size(173, 125);
+            this.credentialsBox.TabIndex = 5;
+            this.credentialsBox.TabStop = false;
             // 
             // tbPassword
             // 
-            this.tbPassword.Location = new System.Drawing.Point(197, 211);
+            this.tbPassword.Location = new System.Drawing.Point(66, 60);
             this.tbPassword.Name = "tbPassword";
             this.tbPassword.Size = new System.Drawing.Size(100, 20);
             this.tbPassword.TabIndex = 1;
             this.tbPassword.UseSystemPasswordChar = true;
             // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(10, 63);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(50, 13);
+            this.lbPassword.TabIndex = 3;
+            this.lbPassword.Text = "Passwort";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(66, 34);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(100, 20);
+            this.tbUser.TabIndex = 0;
+            // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(222, 237);
+            this.btnLogin.Location = new System.Drawing.Point(91, 96);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(75, 23);
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
-            this.btnLogin.Click += new System.EventHandler(this.ButtonLogin_Click);
+            this.btnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // lbUser
+            // 
+            this.lbUser.AutoSize = true;
+            this.lbUser.Location = new System.Drawing.Point(24, 37);
+            this.lbUser.Name = "lbUser";
+            this.lbUser.Size = new System.Drawing.Size(36, 13);
+            this.lbUser.TabIndex = 4;
+            this.lbUser.Text = "E-Mail";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(450, 450);
-            this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.tbPassword);
-            this.Controls.Add(this.tbUser);
-            this.Controls.Add(this.lbPassword);
-            this.Controls.Add(this.lbUser);
+            this.Controls.Add(this.credentialsBox);
             this.Name = "Login";
             this.Text = "Login";
+            this.credentialsBox.ResumeLayout(false);
+            this.credentialsBox.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
+
+        private System.Windows.Forms.GroupBox credentialsBox;
+
+        private System.Windows.Forms.GroupBox groupBox1;
 
         #endregion
 
