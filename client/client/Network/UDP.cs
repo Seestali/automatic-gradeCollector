ee8ba0546a185d631bbe371820bbc03b1a3ee3dd
Henrik Kaltenbach <henrik.kaltenbach49@gmail.com>
2021-11-30 19:28:09 +0100

    Implemented Send and BeginReceive from MS docs

--- client/client/Network/UDP.cs
+++ client/client/Network/UDP.cs
@@ -1,34 +1,75 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace client.Network
 {
     public class UDP
     {
-        private static int port = 42069;
-        private static string serverAddress = "";
-        private static UdpClient udpReceiver = new UdpClient(port);
-        private static IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(serverAddress), port);
+        public const int PORT = 42069;
+        public const string SERVER_DOMAIN = "vollsm.art";
+        public bool packetReceived;
 
-        private UDP() { }
+        private static UDP instance;
+        private readonly UdpClient udpReceiver;
+        private readonly UdpClient udpSender;
+        private IPEndPoint remoteEP;
+        private byte[] receivedBytes;
+
+        private UDP(string serverDomain, int port)
+        {
+            udpReceiver = new UdpClient(port);
+            udpSender = new UdpClient();
+            remoteEP = new IPEndPoint(Dns.GetHostEntry(serverDomain).AddressList[0], port);
+        }
+
+        private UDP(IPAddress serverIP, int port)
+        {
+            udpReceiver = new UdpClient(port);
+            udpSender = new UdpClient();
+            remoteEP = new IPEndPoint(serverIP, port);
+        }
+
+        public static UDP GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new UDP(SERVER_DOMAIN, PORT);
+            }
+            return instance;
+        }
 
         public void Send(byte[] datagram)
         {
-            using (UdpClient c = new UdpClient(port))
-                c.SendAsync(datagram, datagram.Length, serverEP);
+            udpSender.Send(datagram, datagram.Length, remoteEP);
         }
 
-        public void Receive()
+        public void BeginReceive()
         {
             udpReceiver.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+            while (!packetReceived)
+            {
+                Thread.Sleep(100);
+            }
         }
 
-        private void ReceiveCallback(IAsyncResult result)
+        private void ReceiveCallback(IAsyncResult ar)
         {
-            byte[] received = udpReceiver.EndReceive(result, ref serverEP);
-            udpReceiver.BeginReceive(new AsyncCallback(ReceiveCallback), null);
-            // TODO: Received data must be forwarded
+            receivedBytes = udpReceiver.EndReceive(ar, ref remoteEP);
+            Console.WriteLine($"Received: {myToString(receivedBytes)}");
+            packetReceived = true;
+        }
+
+        private string myToString(byte[] bytes)
+        {
+            StringBuilder stringBuilder = new StringBuilder(bytes.Length);
+            foreach (byte b in bytes)
+            {
+                stringBuilder.Append(b);
+            }
+            return stringBuilder.ToString();
         }
     }
 }
