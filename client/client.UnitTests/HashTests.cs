40c6a01af9c6040970039851ff20200974818bbd
Seestali <seebasti@gmx.de>
2021-12-07 18:47:57 +0100

    test project files

---
+++ client/client.UnitTests/HashTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using client;
+using Xunit;
+
+namespace client.UnitTests
+{
+    public class HashTests
+    {
+        [Fact]
+        public void GetHashString_InputString_ReturnsSame()
+        {
+            var hash = new Hash();
+            Assert.True(true);
+        }
+    }
+}
