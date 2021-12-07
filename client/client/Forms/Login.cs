461e260e216a0fdc50fd3bea6442c60d5e72cf17
Seestali <seebasti@gmx.de>
2021-12-07 18:46:46 +0100

    added test project to solution

--- client/client/Forms/Login.cs
+++ client/client/Forms/Login.cs
@@ -7,8 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Security.Cryptography;
 using client.Network;
+using client.utils;
+
 
 namespace client
 {
@@ -22,27 +23,14 @@ namespace client
         {
             InitializeComponent();
         }
-
+        //TODO: Real documentation for function
         private void BtnLogin_Click(object sender, EventArgs e)
         {
-            //TODO: make login and proceed with program (close Login-Form and create new)
-            MessageBox.Show(tbUser.Text + "\n\n" + GetHashString(tbPassword.Text));
-        }
-
-        //TODO: Create separate class for hashing function
-        private static byte[] GetHash(string inputString)
-        {
-            using (HashAlgorithm algorithm = SHA256.Create())
-                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
-        }
-
-        private static string GetHashString(string inputString)
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach (byte hashbyte in GetHash(inputString))
-                //X2-Format: Formats string as two uppercase hexadecimal characters
-                sb.Append(hashbyte.ToString("X2"));
-            return sb.ToString();
+            //MessageBox.Show(tbUser.Text + "\n\n" + Hash.GetHashString(tbPassword.Text));
+            System.Diagnostics.Debug.WriteLine(Hash.GetHashString(tbPassword.Text));
+            //TODO: send packets with credentials to server
+            //TODO: get ACK/DEC back with data
+            //TODO: open MainWindow with received information and show courses and grades
         }
 
         private void btnDebugSendReceive(object sender, EventArgs e)
---
+++ client/client/Utils/Hash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace client.Utils
+{
+    public static class Hash
+    {
+        private static byte[] GetHash(string inputString)
+        {
+            using (HashAlgorithm algorithm = SHA256.Create())
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
+
+        public static string GetHashString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte hashbyte in GetHash(inputString))
+                //X2-Format: Formats string as two uppercase hexadecimal characters
+                sb.Append(hashbyte.ToString("X2"));
+            return sb.ToString();
+        }
+    }
+}
