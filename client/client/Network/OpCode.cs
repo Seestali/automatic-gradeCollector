d9dab23a8f95af0960a5f809028f00b160c96583
Henrik Kaltenbach <henrik.kaltenbach49@gmail.com>
2021-12-12 16:10:58 +0100

    Remove unnecessary usings

--- client/client/Network/OpCode.cs
+++ client/client/Network/OpCode.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace client.Network.Packet
+﻿namespace client.Network
 {
     /// <summary>
     /// Represents the op codes in the protocol.
