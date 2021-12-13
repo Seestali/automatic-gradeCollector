4c5b6644e5980371543d6bf939e579b3d6c4dc20
Henrik Kaltenbach <henrik.kaltenbach49@gmail.com>
2021-12-13 14:56:04 +0100

    Add Test stub for ByteUtil.cs

---
+++ client/client.UnitTests/ByteUtilTest.cs
@@ -0,0 +1,33 @@
+﻿using client.Utils;
+using Xunit;
+
+namespace client.UnitTests
+{
+    public class ByteUtilTest
+    {
+        // TODO:    write tests
+        [Fact]
+        public void InsertUInt32ToByteArray_IntAtArrayBegin_Inplace()
+        {
+
+        }
+
+        [Fact]
+        public void InsertUInt32ToByteArray_IntAtIndex_Inplace()
+        {
+
+        }
+
+        [Fact]
+        public void GetUInt32FromByteArray_IntAtArrayBegin_ReturnsCorrectInt()
+        {
+
+        }
+
+        [Fact]
+        public void GetUInt32FromByteArray_IntAtIndex_ReturnsCorrectInt()
+        {
+
+        }
+    }
+}
