36e12a60198a59ee035f48ed83cdd1d1691d4f82
Henrik Kaltenbach <henrik.kaltenbach49@gmail.com>
2021-12-13 12:34:36 +0100

    Add Todo (collab. w/ S. Trauth)

--- client/client/PacketManager.cs
+++ client/client/PacketManager.cs
@@ -3,13 +3,13 @@
     public class PacketManager
     {
         //TODO: class between UI and PacketAssembler.cs
-        //TODO: reconsider location
+        //TODO: consider relocation
         //TODO: manages user input on interface and instructs assembler class to build right packet
         //TODO: receives orders from UI and forwards constructions to packet assembler class
         //TODO: returns status / information of packets to UI
         //TODO: handle DEC packets and invalid packet
         //TODO: create unittest class
-
+        //TODO: gives ready packets to UDP class
 
     }
 }
