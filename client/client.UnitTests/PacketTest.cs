f9fe5ec7aee00463caf223b7246e9b1f7d950ece
Henrik Kaltenbach <henrik.kaltenbach49@gmail.com>
2021-12-14 16:52:28 +0100

    Commit for Sebastian

---
+++ client/client.UnitTests/PacketTest.cs
@@ -0,0 +1,15 @@
+﻿using client.Network;
+using Xunit;
+
+namespace client.UnitTests
+{
+    public class PacketTest
+    {
+        [Fact]
+        public void CreatePacket_FromByteArray_CorrectPacket()
+        {
+            byte[] data = { 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x03, 0x03, 0x03, 0x03, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00 };
+            //Packet packet = new Packet()
+        }
+    }
+}
