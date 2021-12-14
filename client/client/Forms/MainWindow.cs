2424fa14175f7915f938bd08e1c323684777566c
Seestali <seebasti@gmx.de>
2021-12-14 12:28:11 +0100

    Added TODOs back into MainWindow.cs

--- client/client/Forms/MainWindow.cs
+++ client/client/Forms/MainWindow.cs
@@ -12,6 +12,12 @@ namespace client.Forms
 {
     public partial class MainWindow : Form
     {
+        //TODO: Rename form
+        //TODO: implement ign on google docdes
+        //TODO: class for subjects + grades (subject id for list)
+        //class used for showing disassembled packet and assembling set subjects /grades packet
+        //TODO: implement finish / send function for UI (only when data has been changed)
+        //TODO: dropdownmenu (value = value to build request) + request builder (corresponding button)
         public MainWindow()
         {
             InitializeComponent();
