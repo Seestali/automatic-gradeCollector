4dc178943d63685d8676c24d90cc9edef17b32ac
Henrik Kaltenbach <henrik.kaltenbach49@gmail.com>
2021-12-28 15:48:30 +0100

    Redo changes

--- client/client/Utils/ByteUtil.cs
+++ client/client/Utils/ByteUtil.cs
@@ -1,29 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace client.Utils
+﻿namespace client.Utils
 {
     public class ByteUtil
     {
         public static void InsertUInt32ToByteArray(ref byte[] byteArray, int begin, uint value)
         {
-            byteArray[begin]     = (byte)(value >> 24);
-            byteArray[begin + 1] = (byte)(value >> 16);
-            byteArray[begin + 2] = (byte)(value >> 8);
-            byteArray[begin + 3] = (byte)value;
+            byteArray[begin++] = (byte)(value >> 24);
+            byteArray[begin++] = (byte)(value >> 16);
+            byteArray[begin++] = (byte)(value >> 8);
+            byteArray[begin] = (byte)value;
         }
 
         public static uint GetUInt32FromByteArray(ref byte[] byteArray, int begin)
         {
-            return (uint)(
-                byteArray[begin] << 24 + 
-                byteArray[begin + 1] << 16 + 
-                byteArray[begin + 2] << 8 + 
-                byteArray[begin + 3]
-                );
+            int number = byteArray[begin++] << 24;
+            number += byteArray[begin++] << 16;
+            number += byteArray[begin++] << 8;
+            number += byteArray[begin];
+            return (uint)number;
         }
     }
 }
