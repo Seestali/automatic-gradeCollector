23b147315110bb50aa1bf8f745d8c80b867069c9
Seestali <seebasti@gmx.de>
2021-12-29 16:57:58 +0100

    added new class for better handling subject and grades in Datagrid

---
+++ client/client/Utils/SubjectAndGrade.cs
@@ -0,0 +1,9 @@
+﻿namespace client.Utils
+{
+    public class SubjectAndGrade
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Grade { get; set; }
+    }
+}
